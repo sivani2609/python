2a8a5f3bbc66efc1b9f274faf96a393743e63a1e
sivani2609 <vbsivani.sep26@gmail.com>
2025-08-19 23:36:59 +0530

    Create pattern_1.py

---
+++ pattern_1.py
@@ -0,0 +1,14 @@
+"""
+*
+* *
+* * *
+* * * *
+* * * * *
+"""
+
+rows = int(input("Enter number of rows: "))
+
+for i in range(rows):
+    for j in range(i+1):
+        print("* ", end="")
+    print()
